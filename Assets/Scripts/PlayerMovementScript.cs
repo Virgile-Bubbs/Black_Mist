d3aa266a4839b6ca214ee7a88781c021b98fd7eb
Virgile Aragon <virgile.aragon@etu.univ-lyon1.fr>
2020-07-06 15:51:12 +0200

    first

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    public float moveSpeed = 1.5f;
+    public float jumpForce = 2f;
+
+    private Vector3 moveVertical;
+    private Vector3 moveHorizontal;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+ 
+    private Rigidbody rb;
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        moveVertical = new Vector3(0, 0, Input.GetAxisRaw("Vertical")).normalized;
+        moveHorizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0).normalized;
+
+        rb = GetComponent<Rigidbody>();
+           
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Z))
+            rb.MovePosition(rb.position + transform.TransformDirection(moveVertical) * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            rb.MovePosition(rb.position + transform.TransformDirection(moveVertical) * (moveSpeed/2) * Time.deltaTime);
+        if (Input.GetKey(KeyCode.Q))
+            rb.MovePosition(rb.position + transform.TransformDirection(moveHorizontal) * moveSpeed * Time.deltaTime);    
+        if (Input.GetKey(KeyCode.D))
+            rb.MovePosition(rb.position + transform.TransformDirection(moveHorizontal) * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpForce);
+        }
+
+    }
+}
---
+++ Assets/Scripts/VaisseauController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaisseauController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.AddForce(transform.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            rb.AddForce(-transform.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rb.AddForce(transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(-transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.AddForce(-transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * speed * Time.deltaTime);
+        }
+            
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Gravity")
+        {
+            GetComponent<PlayerGravityBody>().enabled = false;
+            Debug.Log("EXIT");
+        }    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Gravity")
+        {
+            GetComponent<PlayerGravityBody>().enabled = true;
+            Debug.Log("ENTER");
+        }
+    }
+}
